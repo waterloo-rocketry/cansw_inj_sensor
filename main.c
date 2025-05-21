7b45a12dbb9a55bd038bc0f0edecb9a5480dedba
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-05-20 20:50:10 -0400

    wip, finished skeleton for heartbeat, ommitted status_ok for now

--- main.c
+++ main.c
@@ -3,22 +3,49 @@
 
 #include <stdbool.h>
 
-//#include "canlib/canlib.h"
-//#include "canlib/message_types.h"
+#include "canlib/canlib.h"
+#include "canlib/message_types.h"
+
+#include "mcc_generated_files/system/system.h"
 
 #include "sensor_general.h"
 
+#include "rocketlib/include/timer.h"
+
+//#include "IOExpanderDriver.h"
+//#include "actuator.h"
+//#include "error_checks.h"
+//#include "i2c.h"
+
 #include <xc.h>
 
+
+// Set any of these to zero to disable
+#define STATUS_TIME_DIFF_ms 500 // 2 Hz
+
 int main() {
-    while (true) {
-        for (int i = 0; i < 1000; ++i) {
-            continue;
-        }
-        LED_heartbeat_R();
-        for (int i = 0; i < 1000; ++i) {
-            continue;
-        }
+    while (1) {
+        // preface with #if (BOARD_UNIQUE_ID == ??
+        uint32_t last_millis = millis();
+        
+        // heartbeat Red LED to toggle every 500ms if status_ok
+        if (millis() - last_millis > STATUS_TIME_DIFF_ms) {
+
+            // check for general board status
+            bool status_ok = true;
+            // below are likely unnecessary as stated by liz
+//            status_ok &= check_battery_voltage_error(batt_vol_sense);
+//
+//            status_ok &= check_5v_current_error(current_sense_5v);
+//            status_ok &= check_12v_current_error(current_sense_12v);
+
+            // if there was an issue, a message would already have been sent out
+//            if (status_ok) {
+//                send_status_ok();
+//            }
+            
+            // Red LED flashes during safe state.
+            LED_heartbeat_R();
+        } 
     }
-   
 }
